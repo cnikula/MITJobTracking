066d3a65f3ea339096969a3cc27537fc120921b7
Claude R Nikula <cnikula@mesquiteit.com>
2024-07-31 10:07:41 -0500

    Daliy update. Added new SP for query Job information witch inclouds Inventory

---
+++ MITJobTracker/Data/DTOS/JobsInterviewDTO.cs
@@ -0,0 +1,54 @@
+﻿// ***********************************************************************
+// Assembly         : MITJobTracker
+// Author           : Claude Nikula
+// Created          : 07-16-2024
+//
+// Last Modified By : Claude Nikula
+// Last Modified On : 07-16-2024
+// ***********************************************************************
+// <copyright file="JobsInterviewDTO.cs" company="Mesquite IT">
+//     Copyright (c) . All rights reserved.
+// </copyright>
+// <summary>DTO Class merging Jobs and Interview fields</summary>
+// ***********************************************************************
+
+
+namespace MITJobTracker.Data.DTOS
+{
+    public class JobsInterviewDTO
+    {
+
+            public int JobId { get; set; }
+            public string JobTitle { get; set; }
+            public string JobNo { get; set; }
+            public string CompanyName { get; set; }
+            public string RecruitingAgency { get; set; }
+            public string RecruitertName { get; set; }
+            public string RecruiterPhone { get; set; }
+            public string RecruiterEmail { get; set; }
+            public string JobDescription { get; set; }
+            public string JobLocation { get; set; }
+            public bool Remote { get; set; }
+            public bool Hybrid { get; set; }
+            public string HybridNoOfDays { get; set; }
+            public string Requirements { get; set; }
+            public string Salary { get; set; }
+            public string EmploymentType { get; set; }
+            public bool SubContract { get; set; }
+            public bool ResumeSend { get; set; }
+            public DateTime ResumeSendDate { get; set; }
+            public DateTime DateApplied { get; set; }
+            public string Duration { get; set; }
+            public bool OnSite { get; set; }
+            public int InterviewId { get; set; }
+            public DateTime InterviewDate { get; set; }
+            public string InterviewType { get; set; }
+            public string InterviewerName { get; set; }
+            public string InterviewerPhone { get; set; }
+            public string InterviewerEmail { get; set; }
+            public string InterviewerNotes { get; set; }
+            public string InterviewerResulte { get; set; }
+        
+
+    }
+}
---
+++ MITJobTracker/Migrations/20240730191902_sp_GetDetailInfo_Ver1.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MITJobTracker.Migrations
+{
+    /// <inheritdoc />
+    public partial class sp_GetDetailInfo_Ver1 : Migration
+    {
+        string sql = String.Empty;
+
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+                            sql = @"USE MITJobTracker19
+                GO
+
+                IF DB_NAME() <> N'MITJobTracker19' SET NOEXEC ON
+                GO
+
+                SET QUOTED_IDENTIFIER, ANSI_NULLS ON
+                GO
+
+                PRINT (N'Create or alter procedure [dbo].[GetDetailInfo]')
+                GO
+                CREATE OR ALTER PROCEDURE dbo.sp_GetDetailInfo
+                  @Id INTEGER
+                AS 
+                -- ============================================================================
+                 -- Author:      Claude Nikula
+                 -- Create date: 7/30/2024 1:52 PM
+                 -- Database:    MITJobTracker19
+                 -- Description: 
+                 --
+                 -- Parameter:  @Id - primary key for the Jobs table-
+                 --
+                 -- Modify:
+                 -- ===========================================================================
+
+                SET NOCOUNT ON
+
+                SELECT j.JobId
+                      ,j.JobTitle
+                      ,j.JobNo
+                      ,j.CompanyName
+                      ,j.RecruitingAgency
+                      ,j.RecruitertName
+                      ,j.RecruiterPhone
+                      ,j.RecruiterEmail
+                      ,j.JobDescription
+                      ,j.JobLocation
+                      ,j.Remote
+                      ,j.Hybrid
+                      ,j.HybridNoOfDays
+                      ,j.Requirements
+                      ,j.Salary
+                      ,j.EmploymentType
+                      ,j.SubContract
+                      ,j.ResumeSend
+                      ,j.ResumeSendDate
+                      ,j.DateApplied
+                      ,j.Duration
+                      ,j.OnSite
+                      ,i.InterviewId
+                      ,i.InterviewDate
+                      ,i.InterviewType
+                      ,i.InterviewerName
+                      ,i.InterviewerPhone
+                      ,i.InterviewerEmail
+                      ,i.InterviewerNotes
+                      ,i.InterviewerResulte
+                 FROM Jobs j
+                  LEFT JOIN Interviews i
+                    ON j.JobId = i.JobId
+                 WHERE j.JobId = @Id
+
+                GO";
+
+                migrationBuilder.Sql(sql);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            sql = @"USE MITJobTracker19
+                    GO
+
+                    IF DB_NAME() <> N'MITJobTracker19' SET NOEXEC ON
+                    GO
+
+                    --
+                    -- Drop procedure [dbo].[sp_GetDetailInfo]
+                    --
+                    GO
+                    DROP PROCEDURE IF EXISTS dbo.sp_GetDetailInfo
+                    GO";
+
+            migrationBuilder.Sql(sql);
+        }
+    }
+}
