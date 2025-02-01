e42e9b87e8b66bdbbf100de4cb36693a61689e6b
Claude R Nikula <cnikula@mesquiteit.com>
2025-02-01 04:26:00 -0600

    Update read me file

--- MITJobTracker/Program.cs
+++ MITJobTracker/Program.cs
@@ -42,13 +42,9 @@ builder.Services.AddTransient<IJobsServices, JobsServices>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-
-// 25.X.X
-//Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("MzE4NTY5OUAzMjM1MmUzMDJlMzBlMUhoT1ByL0NOczVtcDV4OUprQ3pZT09DbmNPeXR2MU1vUlhzdjJXNC9RPQ==;Mgo+DSMBPh8sVXJxS0d+X1RPd11dXmJWd1p/THNYflR1fV9DaUwxOX1dQl9nSX1RcERgXX9fd3ZcRWc=;ORg4AjUWIQA/Gnt2UFhhQlJBfV5AQmBIYVp/TGpJfl96cVxMZVVBJAtUQF1hTX5QdENiWntecXNXT2Nb;Mgo+DSMBMAY9C3t2UFhhQlJBfV5AQmBIYVp/TGpJfl96cVxMZVVBJAtUQF1hTX5QdENiWntecXNWRmlY;MzE4NTcwM0AzMjM1MmUzMDJlMzBYSW81eHMzb1hQMXF4Z0pYS3NDMDI2ZnA0Skdqc1ZyRVJHcklieVNHMVdRPQ==");
 
 // 26.X.X
-Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1NCaF5cXmZCf1FpRmJGdld5fUVHYVZUTXxaS00DNHVRdkdnWXhfcnVXRmRZUk13WEU=");
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("<Syncfusion.Licensing go here>");
 
 if (!app.Environment.IsDevelopment())
 {
